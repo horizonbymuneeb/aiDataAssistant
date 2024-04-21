acc3de72afe75d0a708030d4b141ca250c95277d
horizonbymuneeb <horizonbymuneeb@gmail.com>
2024-04-21 19:57:10 +0500

    Heading and title and some import issues

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import streamlit as st
 import pandas as pd
 
 from langchain.llms import OpenAI
-from langchain.agents import create_pandas_dataframe_agent
+from langchain_experimental.agents import create_pandas_dataframe_agent
 from dotenv import load_dotenv, find_dotenv
 
+
+#Main
+st.title("AI Assistance for Data Science")
+st.header("Data Analysis Part")
+st.subheader("Solution")
+st.write("This AI assistant can help you with data analysis. Please input your data and the question you have in mind.")
